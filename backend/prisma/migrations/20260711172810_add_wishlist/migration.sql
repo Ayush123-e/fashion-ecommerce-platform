91b1eb3d1f0ffedb99884dea58d240f9982eb501
Ayush Kumar <ayushnbt94@gmail.com>
2026-07-12 00:22:13 +0530

    feat: setup database schema models and migration history

---
+++ backend/prisma/migrations/20260711172810_add_wishlist/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE "WishlistItem" (
+    "userId" TEXT NOT NULL,
+    "productId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "WishlistItem_pkey" PRIMARY KEY ("userId","productId")
+);
+
+-- AddForeignKey
+ALTER TABLE "WishlistItem" ADD CONSTRAINT "WishlistItem_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "WishlistItem" ADD CONSTRAINT "WishlistItem_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE CASCADE ON UPDATE CASCADE;
